4cc8739fd9b5fcca78980c17feb6b626c42281a9
Shashwat <shashwatverma888@gmail.com>
2021-01-27 23:49:32 +0530

    cache static content

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,19 @@ workbox.routing.registerRoute(
     ({request})=> request.destination === "image",
     new workbox.strategies.CacheFirst()
 );
+
+self.addEventListener("install", e=>{
+    e.waitUntil(
+        caches.open("static").then(cache =>{
+            return cache.addAll(["./", "./game.js", "./sounds/wrong.mp3", "./sounds/blue.mp3", "./sounds/green.mp3", "./sounds/red.mp3", "./sounds/yellow.mp3", "./styles.css"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(response=>{
+            return response || fetch(e.request);
+        })
+    );
+});
