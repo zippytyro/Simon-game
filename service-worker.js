95b1480ab354a4969d1b3cab1271a6a90b6f8213
Shashwat <shashwatverma888@gmail.com>
2021-01-28 21:13:29 +0530

    .gitignore and sw edited

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,23 @@
 // jshint esversion:6
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
-workbox.routing.registerRoute(
-    ({request})=> request.destination === "image",
-    new workbox.strategies.CacheFirst()
-);
-
+// workbox.routing.registerRoute(
+//     ({request})=> request.destination === "image",
+//     new workbox.strategies.CacheFirst()
+// );
+const CACHE  = 'Simon-game';
+const cacheWhitelist = ['Simon-game'];
+let resourcesToCache = ["./", "./img/game.png", "./game.js", "./sounds/wrong.mp3", "./sounds/blue.mp3", "./sounds/green.mp3", "./sounds/red.mp3", "./sounds/yellow.mp3", "./styles.css"];
+// To install a service worker
 self.addEventListener("install", e=>{
     e.waitUntil(
-        caches.open("static").then(cache =>{
-            return cache.addAll(["./", "./game.js", "./sounds/wrong.mp3", "./sounds/blue.mp3", "./sounds/green.mp3", "./sounds/red.mp3", "./sounds/yellow.mp3", "./styles.css"]);
+        caches.open(CACHE).then(cache =>{
+            return cache.addAll(resourcesToCache);
         })
     );
 });
 
+// Cache and return requests
 self.addEventListener("fetch", e=>{
     e.respondWith(
         caches.match(e.request).then(response=>{
@@ -21,3 +25,18 @@ self.addEventListener("fetch", e=>{
         })
     );
 });
+
+// Update a service worker
+self.addEventListener('activate', event => {
+    event.waitUntil(
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    );
+  });
