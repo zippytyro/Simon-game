a8fa53dfe2409c1454f84db299979e586def438a
Shashwat <shashwatverma888@gmail.com>
2021-01-29 00:46:32 +0530

    refactor

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,12 @@
 // jshint esversion:6
-// importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
-// workbox.routing.registerRoute(
-//     ({request})=> request.destination === "image",
-//     new workbox.strategies.CacheFirst()
-// );
-const CACHE  = 'Simon-game';
-const cacheWhitelist = ['Simon-game'];
+// Installing service workker
+const CACHE_NAME  = 'Simon-game';
 let resourcesToCache = ["./", "./img/game.png", "./game.js", "./sounds/wrong.mp3", "./sounds/blue.mp3", "./sounds/green.mp3", "./sounds/red.mp3", "./sounds/yellow.mp3", "./styles.css"];
-// To install a service worker
+
 self.addEventListener("install", e=>{
     e.waitUntil(
-        caches.open(CACHE).then(cache =>{
+        caches.open(CACHE_NAME).then(cache =>{
             return cache.addAll(resourcesToCache);
         })
     );
@@ -27,6 +22,7 @@ self.addEventListener("fetch", e=>{
 });
 
 // Update a service worker
+const cacheWhitelist = ['Simon-game'];
 self.addEventListener('activate', event => {
     event.waitUntil(
       caches.keys().then(cacheNames => {
